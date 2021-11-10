5297ff0e725a97a677a93d2b66a6405db7042c80
August Penny <apenny23@apenny23.local>
2021-11-10 11:16:50 -0700

    Added some private methods and instance data to use in fututre features.

--- src/StackNode.java
+++ src/StackNode.java
@@ -1,2 +1,18 @@
 public class StackNode {
+    private int length;
+    private node root;
+
+    public StackNode(){
+        root=new node();
+        length=0;
+    }
+
+    //private methods to make the public ones work.
+
+    private node topNode(node n2){//recursive method to return the current top of the stack
+        if(n2.getPrev()==null){
+            return n2;
+        }
+        return topNode(n2.getPrev());
+    }
 }
