e6a6fde0a7830d6c6e497d12178807f5174640ec
August Penny <apenny23@apenny23.local>
2021-11-11 12:10:18 -0700

    Added a header

--- src/StackNode.java
+++ src/StackNode.java
@@ -1,3 +1,13 @@
+/**
+ * StackNode.java
+ * Makes a stack using the Node class
+ * Author: August Penny
+ * Collaborator(s): The names of anyone you collaborated with here
+ * Collaboration: Describe the collaboration that took place
+ * Date: Date the program is handed in 11/11/21
+ **/
+
+
 public class StackNode {
     private int length;
     private node root;
